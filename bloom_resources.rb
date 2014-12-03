44df122ead0d04b3388ed8867f7f62c69dcaa147
gupiard <guillaume.piard@gmail.com>
2014-12-02 21:59:01 -0600

    new app with gems

---
+++ bloom_resources.rb
@@ -0,0 +1,10 @@
+rails generate starter:resource account user_id:integer category:string
+rails generate starter:resource objective account_id:integer target_portfolio_id:integer name:string category:string target_date:date target_value:float
+rails generate starter:resource target_portfolio risk_rank:integer pea_compatible:boolean expected_return:float standard_deviation:float
+rails generate starter:resource allocation target_portfolio_id:integer etf_id:integer weight:float
+rails generate starter:resource etf name:string ticker:string isin:string asset_class:string currency:string manager:string fee:float
+rails generate starter:resource price etf_id:integer close_date:date close_price:float
+rails generate starter:resource holding objective_id:integer etf_id:integer shares:float
+
+rails generate devise user first_name:string last_name:string dob:date phone:string appartment:string residence:string street:string county:string post_code:string city:string employment_status:string annual_income:float net_worth:float
+rails generate devise:views
