0d24ab835426666ffdcbbcd657289a3b36bf26a3
gupiard <guillaume.piard@gmail.com>
2014-12-10 15:33:09 -0600

    small inputs on sign_up

--- bloom_resources.rb
+++ bloom_resources.rb
@@ -1,3 +1,4 @@
+# Main resources
 rails generate starter:resource account user_id:integer category:string
 rails generate starter:resource objective account_id:integer target_portfolio_id:integer name:string category:string target_date:date target_value:float
 rails generate starter:resource target_portfolio risk_rank:integer pea_compatible:boolean expected_return:float standard_deviation:float
@@ -6,9 +7,14 @@ rails generate starter:resource etf name:string ticker:string isin:string asset_
 rails generate starter:resource price etf_id:integer close_date:date close_price:float
 rails generate starter:resource holding objective_id:integer etf_id:integer shares:float
 
+# Devise User resource with admin boolean
 rails g devise:install
-rails generate devise user first_name:string last_name:string dob:date phone:string appartment:string residence:string street:string county:string post_code:string city:string employment_status:string annual_income:float net_worth:float
+# rails generate devise user first_name:string last_name:string dob:date phone:string appartment:string residence:string street:string county:string post_code:string city:string employment_status:string annual_income:float net_worth:float
+# rails generate migration add_admin_to_users admin:boolean
+rails generate devise user first_name:string last_name:string dob:date phone:string appartment:string residence:string street:string county:string post_code:string city:string employment_status:string annual_income:float net_worth:float admin:boolean
 rails generate devise:views
-rails generate migration add_admin_to_users admin:boolean
 
+# Bootstrap/Bootswatch
 rails g starter:style flatly
+
+
